98e2f4e443d1daa2e2b4f96ad01fad37493f177c
Chiao52 <88649220+Chiao52@users.noreply.github.com>
2023-03-14 11:21:20 +0800

    Update connection.py

--- functions/connection.py
+++ functions/connection.py
@@ -32,7 +32,7 @@ def macos(ovpn_file_content, vpn_hostname, vpn_ip, vpn_country):
     print("\n## The vpngate_{}_{}_{}.ovpn file imported successfully. \n\n===== ## Now, you can connect with OpenVPN ! ===== \n\n".format(vpn_hostname, vpn_country, vpn_ip)) 
 
 def windows(ovpn_file_content, vpn_hostname, vpn_ip, vpn_country):
-    openvpn_path = input("【 Please input your openVPN software file path. 】\n\n E.g. /etc/openvpn \n\n=> ")    
+    openvpn_path = input("【 Please input your openVPN software file path. 】\n\n E.g. C:/ProgramFile/openvpn/config \n\n=> ")    
     while(openvpn_path.strip()==''): 
         print("\n[Sorry, this path information is necessary, please input again.]")
         print('\n-----------------------------------\n')
